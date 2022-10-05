18cbb27cd3f6180f98622d883f60c26a73e7ed14
marthebull <marthebullpettersen@gmail.com>
2022-10-05 18:11:50 +0200

    prøver å få delete knapp

--- js/home-feed.js
+++ js/home-feed.js
@@ -2,6 +2,7 @@ const posts = document.getElementById("posts");
 const postContent = document.getElementById("post-content");
 const postTitle = document.getElementById("post-title");
 const submitPost = document.getElementById("submit-post");
+const yourUsername = document.getElementById("your-username");
 
 const API_BASE_URL = "https://nf-api.onrender.com";
 
@@ -10,6 +11,8 @@ const postPostURL = `${API_BASE_URL}/api/v1/social/posts`;
 
 const allPosts = [];
 
+yourUsername.innerHTML = localStorage.getItem("username");
+
 const writePosts = () => {
   posts.innerHTML = "";
   for (let content of allPosts) {
@@ -17,15 +20,25 @@ const writePosts = () => {
             <div class="bg-white rounded-3 p-3 mb-3">
                 <div>
                     <a class="d-flex align-items-center mb-4 text-decoration-none" href="profile.html">
-                        <p class="font-weight-bold ps-3 mb-0 text-black">${content.author.name}</p>
+                        <p class="font-weight-bold ps-3 mb-0 text-black">${
+                          content.author.name
+                        }</p>
                     </a>
                 </div>
                 <h3>${content.title}</h3>
                 <p>${content.updated}</p>
                 <p>${content.body}</p>
+                ${
+                  content.author === yourUsername
+                    ? "<button>delete</button>"
+                    : ""
+                }
             </div>`;
   }
 };
+
+// sette inn deletknapp som dukker opp på posten IF author er marthebull
+// så posts.innerHTML += <button id=${content.id}></button>
 
 async function getAllPosts(url) {
   try {
@@ -36,18 +49,17 @@ async function getAllPosts(url) {
         Authorization: `Bearer ${accessToken}`,
       },
     };
-    //console.log(url, options);
+    console.log(url, options);
 
     const response = await fetch(url, options);
 
     const posts = await response.json();
+    console.log(posts);
 
-    if (response.status === 200) {
-      for (let post of posts) {
-        allPosts.push(post);
-      }
-      writePosts();
+    for (let post of posts) {
+      allPosts.push(post);
     }
+    writePosts();
   } catch (error) {
     console.warn(error);
   }
@@ -59,6 +71,7 @@ getAllPosts(getAllPostsURL);
 
 async function postPost(url, data) {
   console.log("funger pls");
+
   try {
     const accessToken = localStorage.getItem("accessToken");
     //console.log(accessToken);
@@ -71,12 +84,16 @@ async function postPost(url, data) {
       body: JSON.stringify(data),
     };
     console.log(url, data, options);
-
+    // opp i api
     const response = await fetch(url, options);
     console.log(response);
     const answer = await response.json();
-    if (answer.status === 200) {
+    if (response.status === 200) {
+      console.log("bra");
       getAllPosts(getAllPostsURL);
+      setTimeout(() => {
+        window.location.reload();
+      }, 100);
     }
     console.log(answer);
   } catch (error) {
@@ -90,10 +107,12 @@ submitPost.addEventListener("click", () => {
     body: postContent.value.trim(), // Required
   };
   postPost(postPostURL, postData);
+
+  setTimeout(() => {
+    getAllPosts(getAllPostsURL);
+  }, 5000);
 });
 
 submitPost.addEventListener("click", () => {
   console.log("Så smart marthe");
 });
-
-getAllPosts(getAllPostsURL);
